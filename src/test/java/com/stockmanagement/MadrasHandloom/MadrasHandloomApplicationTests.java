69233ee62533813e9a221982190a8374a0009a33
Shourabh.raja <shourabh.raja@simplilearn.net>
2021-08-24 08:27:02 +0530

    InitialCommit_readable

---
+++ src/test/java/com/stockmanagement/MadrasHandloom/MadrasHandloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockmanagement.MadrasHandloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadrasHandloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
